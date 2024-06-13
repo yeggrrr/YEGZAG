b4af6db69470b9c15c99510284de3236cda765b5
yeggrrr <yjkim981103@gmail.com>
2024-06-13 18:45:57 +0900

    Initial Commit

---
+++ YEGZAG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YEGZAG
+//
+//  Created by YJ on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
