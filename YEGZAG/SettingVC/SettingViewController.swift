5be68aa87a06b033999e55999ac840004a47e9fd
yeggrrr <yjkim981103@gmail.com>
2024-06-16 18:49:41 +0900

    [Feat] SettingVC UI 완성

--- YEGZAG/SettingVC/SettingViewController.swift
+++ YEGZAG/SettingVC/SettingViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 import SnapKit
 
+enum SettingOptions: String, CaseIterable {
+    case wishList = "나의 장바구니 목록"
+    case frequentlyQuestions = "자주 묻는 질문"
+    case personalInquiry = "1:1 문의"
+    case notificationSettings = "알림 설정"
+    case unsubscribe = "탈퇴하기"
+}
+
 class SettingViewController: UIViewController {
     let settingTableView = UITableView()
     
@@ -32,8 +40,6 @@ class SettingViewController: UIViewController {
             $0.edges.equalTo(safeArea)
         }
     }
-    
-    
 }
 
 extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
@@ -53,16 +59,22 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
         if section == 0 {
             return 1
         } else {
-            return 1
+            return SettingOptions.allCases.count
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             guard let profileCell = tableView.dequeueReusableCell(withIdentifier: ProfileTableViewCell.id, for: indexPath) as? ProfileTableViewCell else { return UITableViewCell() }
+            profileCell.selectionStyle = .none
             return profileCell
         } else {
             guard let wishCell = tableView.dequeueReusableCell(withIdentifier: ETCTableViewCell.id, for: indexPath) as? ETCTableViewCell else { return UITableViewCell() }
+            wishCell.wishListLabel.text = SettingOptions.allCases[indexPath.row].rawValue
+            wishCell.selectionStyle = .none
+            if indexPath.row > 0 {
+                wishCell.wishStackView.isHidden  = true
+            }
             return wishCell
         }
     }
