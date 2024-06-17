db5269c5a719484239319271cd69b54a6331b381
yeggrrr <yjkim981103@gmail.com>
2024-06-18 01:31:54 +0900

    [Add] UserDefaultsKey 파일 추가

---
+++ YEGZAG/Storage/UserDefaultsKey.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsKey.swift
+//  YEGZAG
+//
+//  Created by YJ on 6/17/24.
+//
+
+import Foundation
+
+enum UserDefaultsUserInfo: String {
+    case isExistUser
+    case name
+    case profileImage
+    case joinDate
+    case recentSearchList
+    case wishList
+}
