c39d71dd7295590e1ab504215a704038db1c8a4c
yeggrrr <yjkim981103@gmail.com>
2024-07-09 01:34:46 +0900

    [Chore & Feat] 파일 위치 변경 및 EntireWishListTableVC & Cell 생성

---
+++ YEGZAG/WishVC/EntireWishListVC/EntireWishListTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EntireWishListTableViewCell.swift
+//  YEGZAG
+//
+//  Created by YJ on 7/9/24.
+//
+
+import UIKit
+
+class EntireWishListTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ YEGZAG/WishVC/EntireWishListVC/EntireWishListViewController.swift
@@ -0,0 +1,60 @@
+//
+//  EntireWishListViewController.swift
+//  YEGZAG
+//
+//  Created by YJ on 7/9/24.
+//
+
+import UIKit
+import SnapKit
+
+class EntireWishListViewController: UIViewController {
+    let itemListTableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        configureTableView()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(itemListTableView)
+    }
+    
+    func configureLayout() {
+        itemListTableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    func configureTableView() {
+        itemListTableView.delegate = self
+        itemListTableView.dataSource = self
+        itemListTableView.register(EntireWishListTableViewCell.self, forCellReuseIdentifier: EntireWishListTableViewCell.id)
+    }
+    
+}
+
+extension EntireWishListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: EntireWishListTableViewCell.id, for: indexPath) as? EntireWishListTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    
+}
